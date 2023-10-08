ad5a19f59f9a21819772117ccafefe502ce61246
IMP1ZA <chakra36042@gmail.com>
2023-10-08 21:06:20 +0700

    Update meteor movement method
    Change movement method in meteor from transform translate to rb.velocity

--- Assets/Script/Meteor.cs
+++ Assets/Script/Meteor.cs
@@ -7,21 +7,25 @@ public class Meteor : MonoBehaviour
     [SerializeField] private float _moveSpeed = 1.0f;
     [SerializeField] private string _tag;
     private UIManager _manager;
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
         _manager = FindObjectOfType<UIManager>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+        Vector3 movement = new Vector3(-_moveSpeed, 0, 0);
+
+        _rb.velocity = movement;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Bullet")) 
+        if (collision.collider.CompareTag(_tag)) 
         {
             _manager.GetScore();
             Destroy(gameObject);       
